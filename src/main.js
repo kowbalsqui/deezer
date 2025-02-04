6a317d258ab96242329e4ebb3f96d6a53abcd230
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2025-02-04 13:04:31 +0100

    terminal

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/my_boostrap.scss'
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
